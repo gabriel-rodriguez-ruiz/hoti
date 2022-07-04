50e701e15ee946334eda0068836d706d8a3e6ecd
Gabriel <gabrielrr96@gmail.com>
2022-07-04 11:12:40 -0300

    I have changed a sign in diagonal part of 2D A1u and I wrote a semi infinite version.

--- hamiltonians.py
+++ hamiltonians.py
@@ -19,6 +19,19 @@ def index(i, j, alpha, L_x, L_y):
       raise Exception("Site index should not be greater than samplesize.")
   return alpha + 4*( L_y*(i-1) + j - 1)
 
+def index_semi_infinite(i, alpha, L_x):
+  """Return the index of basis vector given the site i
+  and spin index alpha for i in {1, ..., L_x}
+  
+  .. math ::
+     (c_{1}, c_{2}, ..., c_{L_x})^T
+     
+  """
+  if i>L_x:
+      raise Exception("Site index should not be greater than samplesize.")
+  return alpha + 4*(i - 1)
+
+
 def Hamiltonian_A1u(t, mu, L_x, L_y, Delta):
     r"""Return the matrix for A1u model with:
 
@@ -39,8 +52,9 @@ def Hamiltonian_A1u(t, mu, L_x, L_y, Delta):
     M = np.zeros((4*L_x*L_y, 4*L_x*L_y), dtype=complex)
     for i in range(1, L_x+1):
       for j in range(1, L_y+1):
-        for alpha in range(4):
+        for alpha in range(2):
             M[index(i, j, alpha, L_x, L_y), index(i, j, alpha, L_x, L_y)] = -mu/4   # para no duplicar al sumar la traspuesta 
+            M[index(i, j, alpha+2, L_x, L_y), index(i, j, alpha+2, L_x, L_y)] = mu/4   # para no duplicar al sumar la traspuesta 
     for i in range(1, L_x):
       for j in range(1, L_y+1):      
         M[index(i, j, 0, L_x, L_y), index(i+1, j, 0, L_x, L_y)] = -t/2
@@ -61,6 +75,36 @@ def Hamiltonian_A1u(t, mu, L_x, L_y, Delta):
         M[index(i, j, 1, L_x, L_y), index(i, j+1, 2, L_x, L_y)] = Delta/4
         M[index(i, j, 2, L_x, L_y), index(i, j+1, 1, L_x, L_y)] = -Delta/4
         M[index(i, j, 3, L_x, L_y), index(i, j+1, 0, L_x, L_y)] = Delta/4
+    return M + M.conj().T
+
+def Hamiltonian_A1u_semi_infinit(k, t, mu, L_x, L_y, Delta):
+    r"""Returns the H_k matrix for A1u model with:
+
+    .. math::
+        H_{A1u} = \frac{1}{2}\sum_k H_k
+        
+        H_k = \sum_n^L \vec{c}^\dagger_n\left[ 
+            \xi_k\tau_z\sigma_0 +
+            \Delta sin(k_y)\tau_x\sigma_y \right] +
+            \sum_n^{L-1}\vec{c}^\dagger_n(-t\tau_z\sigma_0 + \frac{\Delta}{2i}\tau_x\sigma_x)\vec{c}_{n+1}
+            + H.c.
+            
+       \vec{c} = (c_{k,\uparrow}, c_{k,\downarrow},c^\dagger_{-k,\downarrow},-c^\dagger_{-k,\uparrow})^T
+    """
+    M = np.zeros((4*L_x*L_y, 4*L_x*L_y), dtype=complex)
+    for i in range(1, L_x+1):
+        for alpha in range(2):
+            M[index_semi_infinite(i, alpha, L_x), index_semi_infinite(i, alpha, L_x)] = -t/2*np.cos(k) - mu/4   # para no duplicar al sumar la traspuesta 
+            M[index_semi_infinite(i, alpha+2, L_x), index_semi_infinite(i, alpha+2, L_x)] = t/2*np.cos(k) + mu/4
+    for i in range(1, L_x):
+            M[index_semi_infinite(i, 0, L_x), index_semi_infinite(i+1, 0, L_x)] = -t/2
+            M[index_semi_infinite(i, 1, L_x), index_semi_infinite(i+1, 1, L_x)] = -t/2   
+            M[index_semi_infinite(i, 2, L_x), index_semi_infinite(i+1, 2, L_x)] = t/2
+            M[index_semi_infinite(i, 3, L_x), index_semi_infinite(i+1, 3, L_x)] = t/2  
+            M[index_semi_infinite(i, 0, L_x), index_semi_infinite(i+1, 3, L_x)] = -1j*Delta/2  
+            M[index_semi_infinite(i, 1, L_x), index_semi_infinite(i+1, 2, L_x)] = -1j*Delta/2  
+            M[index_semi_infinite(i, 2, L_x), index_semi_infinite(i+1, 1, L_x)] = -1j*Delta/2  
+            M[index_semi_infinite(i, 3, L_x), index_semi_infinite(i+1, 0, L_x)] = -1j*Delta/2  
     return M + M.conj().T
 
 #%%
