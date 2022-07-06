8c5bc2a877fa81b2db7a48679ab4d19120e76be7
Gabriel <gabrielrr96@gmail.com>
2022-07-06 18:05:54 -0300

    I have changed the reference axis on imshow square plot.

--- A1u_square.py
+++ A1u_square.py
@@ -9,12 +9,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from hamiltonians import Hamiltonian_A1u, Zeeman
 
-L_x = 20
+L_x = 30
 L_y = 20
 t = 1
 Delta = 1
 mu = -2
-Delta_Z = 0.5#0.2
+Delta_Z = 0#0.2
 theta = 0
 
 params = dict(t=t, mu=mu, Delta=Delta,
@@ -38,16 +38,17 @@ probability_density = np.zeros((L_x,L_y, 4))
 for i in range(4):      #each list has 4 elements corresponding to the 4 degenerated energies
     probability_density[:,:,i] = np.abs(creation_up[i])**2 + np.abs(creation_down[i])**2 + np.abs(destruction_down[i])**2 + np.abs(destruction_up[i])**2
 
-plt.figure("Zeeman", clear=True)
-plt.imshow(probability_density[:,:,2], cmap="Blues")
-plt.colorbar()
-plt.title(f"{params}")
-
+fig, ax = plt.subplots(num="Zeeman", clear=True)
+image = ax.imshow(probability_density[:,:,2].T, cmap="Blues", origin="lower") #I have made the transpose and changed the origin to have xy axes as usually
+plt.colorbar(image)
+ax.set_title(f"{params}")
+ax.set_xlabel("x")
+ax.set_ylabel("y")
 #plt.plot(probability_density[10,:,0])
 
 #%% Energies
 
 plt.figure("Energies", clear=True)
 plt.scatter(np.arange(0, len(eigenvalues), 1), eigenvalues)
-plt.xlim([790, 810])
+plt.xlim([2*(L_x*L_y-5), 2*(L_x*L_y+5)])
 plt.ylim([-0.1, 0.1])
