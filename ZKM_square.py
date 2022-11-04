d9e2ac2752d4418549170cca1b68355d2143ac94
Gabriel <gabrielrr96@gmail.com>
2022-11-04 13:53:16 -0300

    I am not sure about the spin measuring.

--- ZKM_square.py
+++ ZKM_square.py
@@ -10,23 +10,29 @@ import matplotlib.pyplot as plt
 from hamiltonians import Hamiltonian_ZKM, Zeeman
 from functions import probability_density
 
-L_x = 30
-L_y = 30
+L_x = 20
+L_y = 80
 t = 1
 mu = -2*t   # mu=t*Delta_0/Delta_1
 Delta_Z = 0  # 0.2
 theta = np.pi/2
-phi = np.pi/4
+phi = 0
 Delta_0 = -0.4*t
 Delta_1 = 0.2*t
 Lambda = 0.5*t
 index = 1
 
-H = Hamiltonian_ZKM(t=t, mu=mu, L_x=L_x, L_y=L_y, Delta_0=Delta_0, Delta_1=Delta_1, Lambda=Lambda)
+#Aligia
+# Delta_0 = 4  #-0.4*t
+# Delta_1 = 2.2  #0.2*t
+# Lambda = 7
+
+H = (Hamiltonian_ZKM(t=t, mu=mu, L_x=L_x, L_y=L_y, Delta_0=Delta_0, Delta_1=Delta_1, Lambda=Lambda) +
+        Zeeman(theta=theta, Delta_Z=Delta_Z, L_x=L_x, L_y=L_y, phi=phi))
 probability_density_2D, eigenvalues, eigenvectors = probability_density(H, L_x, L_y, index=index)
 
 #%%
-fig, ax = plt.subplots(num="Zeeman", clear=True)
+fig, ax = plt.subplots()
 image = ax.imshow(probability_density_2D, cmap="Blues", origin="lower")
 #ax.set_title(f"{params}")
 ax.set_xlabel("x")
@@ -43,6 +49,8 @@ from functions import mean_spin_xy, get_components
 
 zero_modes = eigenvectors[:, 2*(L_x*L_y-1):2*(L_x*L_y+1)]      #4 (2) modes with zero energy (with Zeeman)
 creation_up, creation_down, destruction_down, destruction_up = get_components(zero_modes[:,index], L_x, L_y)
+
+
 zero_plus_state = np.stack((destruction_up, destruction_down, creation_down, creation_up), axis=2) #positive energy eigenvector splitted in components
 # corner_state = zero_plus_state[L_x-2, L_y//2, :].reshape(4,1)  #positive energy point state localized at the junction
 # corner_state_normalized = corner_state/np.linalg.norm(corner_state[:2]) #normalization with only particle part
@@ -69,10 +77,23 @@ u = spin[:, :, 0]   #x component
 v = spin[:, :, 1]   #y component
 
 # Plotting Vector Field with QUIVER
-ax.quiver(x, y, u, v, color='r')
+ax.quiver(x, y, u, v, color='r',angles='xy', scale_units='xy', scale=1)
 ax.set_title('Spin Field in the plane')
 
 #%% Spin in z
 fig, ax = plt.subplots()
+ax.set_title("Spin in z")
 image = ax.imshow(spin[:,:,2], cmap="Blues", origin="lower") #I have made the transpose and changed the origin to have xy axes as usually
 plt.colorbar(image)
+
+fig, ax = plt.subplots()
+ax.set_title("Spin in z")
+ax.plot(spin[:,L_x-1,2])
+
+fig, ax = plt.subplots()
+ax.set_title("Spin in x")
+ax.plot(spin[:,L_x-1,0])
+
+fig, ax = plt.subplots()
+ax.set_title("Spin in y")
+ax.plot(spin[:,L_x-1,1])
